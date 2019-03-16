847ed21047571169405fb0eb4528dfa090244c14
Mika88 <mikaspivak@gmail.com>
2019-03-16 20:37:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ function scuberGreetingForFeet(someNumber) {
   else if (someNumber > 2000) {
     result = 'I will gladly take your thirty bucks.';
   }
+  else if (someNumber > 2500)
+  {
+    result = 'No can do.';
+  }
   return result
 }
