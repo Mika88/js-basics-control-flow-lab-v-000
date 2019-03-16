bca63a077a8f601d167b3a2ade702ff0fc841752
Mika88 <mikaspivak@gmail.com>
2019-03-16 20:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// Write your code in this file!
-function scuberGreetingForFeet(feetNumber) {
-  let output
-  if (feetNumber <= 400) {
-    ouput = 'This one is on me!';
+
+function scuberGreetingForFeet(someNumber) {
+  //everything I want my function to do I put inside these curly braces
+  //in this example, lets say I want my function, addFive, to add 5 to any number I pass in (someNumber), but only IF the number is greater than zero
+  let result
+  if (someNumber <= 400) {
+    result = 'This one is on me!';
   }
-  return output
+  //at the end, if I want my function to return something, I need to state it:
+  return result
 }
